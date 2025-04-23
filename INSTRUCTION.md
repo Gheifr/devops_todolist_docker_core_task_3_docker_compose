688e3aeb75fe63a3e8407f83691490d6fecb4635
Vitalii Prushak <v.prushak@gmail.com>
2025-04-23 20:45:18 +0300

    Solution attempt 4

---
+++ INSTRUCTION.md
@@ -0,0 +1,5 @@
+1. To start containers simply use command:
+docker-compose up -d
+
+2. To stop containers use command:
+docker-compose down
---
+++ init.sql
@@ -0,0 +1,13 @@
+SELECT COUNT(*) INTO @user_exists
+FROM mysql.user
+WHERE user = '${MYSQL_USER}' AND host = '%';
+
+
+IF @user_exists > 0 THEN
+    ALTER USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+ELSE
+    CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+END IF;
+
+
+GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'.* TO '${MYSQL_USER}'@'%';
